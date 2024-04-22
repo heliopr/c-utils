f11f028751785191a2e6092ac897aefe31a4651b
Helio P.R <helioprorep@gmail.com>
2024-04-22 10:35:19 -0300

    sorting test

--- src/tests.c
+++ src/tests.c
@@ -2,25 +2,42 @@
 
 #include "ds/list.h"
 
-int main() {
-    list *l = list_int_new();
+int test_sorting() {
     int n;
-    scanf("%d", &n);
+    while (scanf("%d", &n) && n) {
+        list *l = list_int_new();
+        list *l2 = list_int_new();
+        for (int i = 0; i < n; i++) {
+            int x;
+            scanf("%d", &x);
+            list_append_int(l, x);
+        }
 
-    for (int i = 0; i < n; i++) {
-        int x;
-        scanf("%d", &x);
-        list_append_int(l, x);
+        for (int i = 0; i < n; i++) {
+            int x;
+            scanf("%d", &x);
+            list_append_int(l2, x);
+        }
+
+        list_sort(l, list_cmp_int);
+
+        bool falhou = false;
+        for (int i = 0; i < n && !falhou; i++) {
+            if (list_get_int(l, i) != list_get_int(l2, i)) {
+                printf("FALHOU\n");
+                falhou = true;
+                break;
+            }
+        }
+        
+        if (!falhou) {
+            printf("DEU CERTO\n");
+        }
     }
-
-    printf(": ");
-
-    LIST_FOREACH_INT(x, l) {
-        printf("%d ", *x);
-    }
-    printf("\n");
-
-    list_destroy(l);
 
     return 0;
+}
+
+int main() {
+    
 }
